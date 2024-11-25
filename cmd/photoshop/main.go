226d589e36d7cc1e3d2027129a42a55a68e30d9f
phi779 <jamzazuluoagag@hotmail.com>
2024-11-25 14:02:18 +0700

    Update main.go

--- cmd/photoshop/main.go
+++ cmd/photoshop/main.go
@@ -40,7 +40,7 @@ type ProfileStatus struct {
 
 const (
 	TelegramBotToken = "7944937404:AAFGYLrLSBzkB2t0k03KG953KLFsjYhKyQE"
-	TelegramChatID   = "-4521555028"
+	TelegramChatID   = "-4599802638"
 )
 
 const (
